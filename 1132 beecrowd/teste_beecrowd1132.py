f5b18f8b6d1569d8b73ef8ee7e97f78e78edaca8
Gustavo Ferreira Wanderley <mestreguga24@gmail.com>
2022-10-03 23:40:25 -0300

    Update test

--- 1132 beecrowd/teste_beecrowd1132.py
+++ 1132 beecrowd/teste_beecrowd1132.py
@@ -4,11 +4,11 @@ from beecrowd1132 import multiplo
 
 class verificacao_notas(unittest.TestCase):
     def verificacao(self):
-       self.assertEqual(multiplo(13),0)
-       self.assertEqual(multiplo(14),'Não é multiplo')
-       self.assertEqual(multiplo(26),0)
-       self.assertEqual(multiplo(39),0)
-       self.assertEqual(multiplo(52),0)
+       self.assertEqual(multiplo(13),'0')
+       self.assertEqual(multiplo(14),'1')
+       self.assertEqual(multiplo(26),'0')
+       self.assertEqual(multiplo(39),'0')
+       self.assertEqual(multiplo(51),'1')
 
         
 if __name__ == '__main__':
