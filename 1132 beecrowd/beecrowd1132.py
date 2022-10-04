9eb11242da567669cfd0917175b6a8663b123044
Gustavo Ferreira Wanderley <mestreguga24@gmail.com>
2022-10-03 23:47:35 -0300

    Update Test

--- 1132 beecrowd/beecrowd1132.py
+++ 1132 beecrowd/beecrowd1132.py
@@ -1,6 +1,9 @@
-def multiplo(x):
-    x = int(x)
-    if x%13 == 0:
-        print('0')
-    else:
-        print('1')
+def soma_muts(a, b):
+    soma = 0
+    if a > b:
+        a,b =b,a    
+    for loop in range(a,b+1):
+        if not (loop%13==0):
+            soma=soma+loop
+    return soma
+print (soma_muts(500,550))
--- 1132 beecrowd/teste_beecrowd1132.py
+++ 1132 beecrowd/teste_beecrowd1132.py
@@ -1,14 +1,14 @@
 import unittest
 
-from beecrowd1132 import multiplo
+from beecrowd1132 import soma_muts
 
 class verificacao_notas(unittest.TestCase):
-    def verificacao(self):
-       self.assertEqual(multiplo(13),'0')
-       self.assertEqual(multiplo(14),'1')
-       self.assertEqual(multiplo(26),'0')
-       self.assertEqual(multiplo(39),'0')
-       self.assertEqual(multiplo(51),'1')
+   def test_soma_multiplos(self):
+        self.assertEqual(soma_muts(0, 0), 0)
+        self.assertEqual(soma_muts(100, 101), 201)
+        self.assertEqual(soma_muts(200, 87), 15072)
+        self.assertEqual(soma_muts(-137, -243), -18822)
+        self.assertEqual(soma_muts(-15, 0), -107)
 
         
 if __name__ == '__main__':
